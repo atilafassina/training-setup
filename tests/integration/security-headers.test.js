c941be2fe8c3c97ebd155f592915164644e1a3d7
Atila Fassina <atila@fassina.eu>
2023-05-25 17:36:57 +0200

    add integration tests locally

---
+++ tests/integration/security-headers.test.js
@@ -0,0 +1,26 @@
+import 'isomorphic-fetch'
+import { csp } from '../../lib/csp'
+import { securityHeaders } from '../../lib/security-headers'
+
+describe('Check if Security Headers are defined', () => {
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : process.env.PREVIEW_URL
+
+  test('test if Content-Security-Policy is set on root', async () => {
+    const ping = await fetch(url)
+    const cspHeader = ping.headers.get('Content-Security-Policy-Report-Only')
+
+    expect(cspHeader).toMatch(csp)
+  })
+
+  Object.entries(securityHeaders).forEach(([headerName, headerConfigValue]) => {
+    test(`test if ${headerName} is set on root`, async () => {
+      const ping = await fetch(url)
+      const responseHeaderValue = ping.headers.get(headerName)
+
+      expect(responseHeaderValue).toMatch(headerConfigValue)
+    })
+  })
+})
