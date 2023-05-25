e6485a156ec5d7030db2641bee45e2568ce7847e
Atila Fassina <atila@fassina.eu>
2023-05-25 17:27:20 +0200

    add unit tests / fix jsconfig.json

--- components/layouts/default.jsx
+++ components/layouts/default.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import MetLogo from '../met-logo'
-import { LanguageSwitcher } from './language-switcher'
+import { LanguageSwitcher } from '../language-switcher'
 
 export function DefaultLayout({ children }) {
   return (
